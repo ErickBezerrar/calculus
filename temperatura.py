023ebe15db07ad742a36d2026e6b632a5089be6c
ErickBezerrar <erickbrtrindade@gmail.com>
2022-12-10 13:17:02 -0300

    atualizações

--- temperatura.py
+++ temperatura.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
 import numpy as np
+from sympy import*
 import math
+import matplotlib.pyplot as plt
 
 lista_temperatura = [75.3, 77, 83.2, 84.8, 86.5, 86.4, 81.1, 78.6]
 
@@ -9,7 +10,7 @@ def funcao(x):
     return funcao
 
 
-def trapz(f,a, b, N):
+def metodo_trapezio(f,a, b, N):
     h = ((b+1)-a)
     soma = 0
     for k in range(0, h):
@@ -20,15 +21,15 @@ a = 9
 b = 16
 N = 8
 
-resultado = trapz(funcao, a, b, N)
-print(resultado)
+valor = metodo_trapezio(funcao, a, b, N)
+print(valor)
 lista_hora = [9, 10, 11, 12, 13, 14, 15, 16]
 
 x=10
 ax = plt.axes() 
 ax.set_facecolor("#490a3d") 
 plt.plot(lista_hora, lista_temperatura,label='TEMPERATURAS AO LONGO DO DIA', color='#bd1550',marker='o')
-plt.plot(x, resultado, label='MÉDIA DAS TEMPERATURAS',color='#f8ca00', marker='o')
+plt.plot(x, valor, label='MÉDIA DAS TEMPERATURAS',color='#f8ca00', marker='o')
 plt.ylabel('Tem/Fahrenheit')
 plt.xlabel('Horas')
 plt.axis(ymin=50,ymax=120)
