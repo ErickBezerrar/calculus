afa8ec420f3f68e4a7ad65d6b13e1358e31853bb
ErickBezerrar <erickbrtrindade@gmail.com>
2022-12-10 21:17:30 -0300

    Questão do quadrante de Gauss(possivelmente resolvida)

--- quadraturagauss.py
+++ quadraturagauss.py
@@ -5,8 +5,10 @@ a = 0
 b = 4
 N = 4; 
 x = np.linspace(a,b,N)
-y=(4*x)
+y=((16-x**2)**(1/2))
 integral = 0.0
 for k in range(len(x) - 1):
     integral +=0.5*((x[k+1]-x[k])*(y[k+1]+y[k]))
-print(integral)
+print("Area do primeiro quadrante:", integral)
+
+print("Area total aproximada:", integral * 4)
